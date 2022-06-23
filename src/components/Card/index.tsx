2127547409ef7241b6b945c09dac891cfbf5e742
Gabriel <bellsena@outlook.com>
2022-06-23 11:14:53 -0300

    migrando para typescript

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,9 @@
-export function Card({name, time}) {
+export type CardProps = {
+  name: string;
+  time: string
+}
+
+export function Card({name, time}: CardProps) {
   return (
     <div className="h-16 p-6 w-1/2 bg-purple-500 text-white rounded-[10px] mb-5 flex items-center justify-between duration-300 hover:bg-purple-600">
       <strong className="text-lg">{name}</strong>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { Card } from '../../components/Card'
+import { Card, CardProps } from '../../components/Card'
+
+type ProfileResponse = {
+  name: string
+  avatar_url: string
+}
+
+type User = {
+  name: string
+  avatar: string
+}
 
 export function Home() {
-  const [studentName, setStudentName] = useState()
-  const [students, setStudents] = useState([])
-  const [user, setUser] = useState({ name: '', avatar: '' })
+  const [studentName, setStudentName] = useState('')
+  const [students, setStudents] = useState<CardProps[]>([])
+  const [user, setUser] = useState<User>({} as User)
 
   function handleAddStudent() {
     const newStudent = {
@@ -23,7 +33,7 @@ export function Home() {
       const response = await fetch(
         'https://api.github.com/users/stardusteight-d4c'
       )
-      const data = await response.json()
+      const data = await response.json() as ProfileResponse
       setUser({
         name: data.name,
         avatar: data.avatar_url,
